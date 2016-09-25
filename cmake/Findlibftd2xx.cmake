00c3368389d5cdd715590dfd8aa8a6b4e5de90d5
letrend <simon.trendel@tum.de>
2016-09-25 12:52:04 +0200

    forgot find ftd2xx file somehow

---
+++ cmake/Findlibftd2xx.cmake
@@ -0,0 +1,35 @@
+###############################################################################
+# Find libftd2xx
+#
+# This sets the following variables:
+# libftd2xx_FOUND - True if libftd2xx was found.
+# libftd2xx_INCLUDE_DIRS - headers
+# libftd2xx_LIBRARY - library
+
+FIND_LIBRARY(libftd2xx_LIBRARY NAMES ftd2xx
+	PATHS ${PROJECT_SOURCE_DIR}/../flexrayusbinterface/lib/libftd2xx-x86_64-1.3.6/build/
+)
+
+FIND_PATH(libftd2xx_INCLUDE_DIRS NAMES ftd2xx.h 
+	PATHS ${PROJECT_SOURCE_DIR}/../flexrayusbinterface/lib/libftd2xx-x86_64-1.3.6/ 
+)
+
+MARK_AS_ADVANCED(
+  libftd2xx_INCLUDE_DIRS
+  libftd2xx_LIBRARY)
+
+SET( libftd2xx_FOUND "NO" )
+IF(libftd2xx_INCLUDE_DIRS)
+  MESSAGE(STATUS "libftd2xx include dir: ${libftd2xx_INCLUDE_DIRS}" )
+  INCLUDE_DIRECTORIES( ${libftd2xx_INCLUDE_DIRS} )
+  IF(libftd2xx_LIBRARY)
+    MESSAGE(STATUS "libftd2xx library: ${libftd2xx_LIBRARY}" )
+    SET( libftd2xx_FOUND "YES" )
+  ENDIF(libftd2xx_LIBRARY)
+ENDIF(libftd2xx_INCLUDE_DIRS)
+
+IF(libftd2xx_FOUND)
+    MESSAGE(STATUS "Found libftd2xx library")
+ELSE(libftd2xx_FOUND)
+    MESSAGE(FATAL_ERROR "Could not find libftd2xx")
+ENDIF(libftd2xx_FOUND)
