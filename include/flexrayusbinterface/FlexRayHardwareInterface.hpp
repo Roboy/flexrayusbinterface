e24bba6e223418cffb7cb0dffc29c7c5bd3bf5f3
Victor Savu <vs@generalinterfaces.com>
2016-12-21 16:48:21 +0100

    Remove unused RecordingAction

--- include/flexrayusbinterface/FlexRayHardwareInterface.hpp
+++ include/flexrayusbinterface/FlexRayHardwareInterface.hpp
@@ -23,14 +23,6 @@ public:
     Hard
   };
 
-  enum class RecordingAction
-  {
-    Stop,
-    Play,
-    Pause,
-    Rewind
-  };
-
   /**
   * This function resets the sensor displacement of one motor
   */
